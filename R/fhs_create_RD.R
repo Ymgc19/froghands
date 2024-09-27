902a3a5cf50936c74d3faa9226b33e5bfd51686b
Ymgc19 <username@example.com>
2024-09-27 23:36:24 +0900

    引数を改善

--- R/fhs_create_RD.R
+++ R/fhs_create_RD.R
@@ -4,15 +4,15 @@
 
 # 効用関数を受け取り，レプリケータダイナミクスの勾配をreturn
 fhs_create_RD <- function(u_func1, u_func2, u_func3, u_func1_input = c(...), u_func2_input = c(...), u_func3_input){
-  RD_fradient <- function(p_sector1, p_sector2, p_sector3){
+  RD_fradient <- function(sector1, sector2, sector3){
     # 社会全体の平均利得の計算
-    u_mean <- p_sector1*do.call(u_func1, as.list(u_func1_input)) + 
-              p_sector2*do.call(u_func2, as.list(u_func2_input)) + 
-              p_sector3*do.call(u_func3, as.list(u_func3_input))
+    u_mean <- sector1*do.call(u_func1, as.list(u_func1_input)) +
+              sector2*do.call(u_func2, as.list(u_func2_input)) +
+              sector3*do.call(u_func3, as.list(u_func3_input))
     # それぞれの微分方程式を定義
-    d_sector1 <- p_sector1*(do.call(u_func1, as.list(u_func1_input)) - u_mean)
-    d_sector2 <- p_sector2*(do.call(u_func2, as.list(u_func2_input)) - u_mean)
-    d_sector3 <- p_sector3*(do.call(u_func3, as.list(u_func3_input)) - u_mean)
+    d_sector1 <- sector1*(do.call(u_func1, as.list(u_func1_input)) - u_mean)
+    d_sector2 <- sector2*(do.call(u_func2, as.list(u_func2_input)) - u_mean)
+    d_sector3 <- sector3*(do.call(u_func3, as.list(u_func3_input)) - u_mean)
     return(c(d_sector1, d_sector2, d_sector3, u_mean))
   }
 }
--- test.R
+++ test.R
@@ -1,11 +1,13 @@
+devtools::install_github("Ymgc19/froghands", force = TRUE)
+
 # 効用関数の定義
 # 地元に残って農業をしない
-u_A <- function(sector1, sector2, P, S){
+u_A <- function(P, S){
   return(P + S^(sector1 + sector2))
 }
 
 # 地元に残って農業する
-u_B <- function(sector1, sector2, Q, S){
+u_B <- function(Q, S){
   return(Q^(sector2) + S^(sector1 + sector2))
 }
 
@@ -14,3 +16,6 @@ u_C <- function(R){
   return(R)
 }
 
+library(froghands)
+func <- fhs_create_RD(u_A, u_B, u_C, c(0, 0), c(1, 0), c(2))
+fhs_plot_simplex(0.1, 0.3, 0.6)
